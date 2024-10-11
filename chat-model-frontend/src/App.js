f61ad88964edfcfd394d53f66fdcb9186c772100
Aaron <amramirez1222@gmail.com>
2024-10-11 15:58:16 +0800

    clear user box after send
    now after you hit send it will clear the user box was not clearing it before because of API logic

--- chat-model-frontend/src/App.js
+++ chat-model-frontend/src/App.js
@@ -10,6 +10,9 @@ function App() {
     if (!input) return;
 
     const userMessage = { text: input, sender: "user" };
+
+    setInput("");
+
     setMessages([...messages, userMessage]);
 
     try {
@@ -60,26 +63,5 @@ function App() {
     </div>
   );
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
